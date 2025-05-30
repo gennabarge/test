dae552fc0f06637d53538c3aabbc9a54e6194550
Genna Barge <genna@bostondata.co>
2025-05-30 19:39:37 +0000

    added descriptions to stg_jaffle_shop_customers and stg_jaffle_shop_orders

--- models/staging/stripe/stg_stripe_payments.sql
+++ models/staging/stripe/stg_stripe_payments.sql
@@ -1,5 +1,11 @@
-select 
-    id as customer_id,
+select
+    id as payment_id,
     orderid as order_id,
-    amount
-from raw.stripe.payment
+    paymentmethod as payment_method,
+    status,
+
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
+
+from {{ source('stripe', 'payment') }}
